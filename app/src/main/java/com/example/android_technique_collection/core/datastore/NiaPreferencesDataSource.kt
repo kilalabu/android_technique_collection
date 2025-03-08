7bb4e86cbd877631570db3cd216011b87785e4c1
kuwa <h.kuwabara1103@gmail.com>
2025-03-08 13:56:20 +0900

    ストリームの形にする

--- app/src/main/java/com/example/android_technique_collection/core/datastore/NiaPreferencesDataSource.kt
+++ app/src/main/java/com/example/android_technique_collection/core/datastore/NiaPreferencesDataSource.kt
@@ -4,21 +4,23 @@ import com.example.android_technique_collection.core.model.DarkThemeConfig
 import com.example.android_technique_collection.core.model.ThemeBrand
 import com.example.android_technique_collection.core.model.UserData
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class NiaPreferencesDataSource @Inject constructor(
 ) {
     // 本来はDataStoreから取得する
-    val userData: Flow<UserData> = flowOf(
-        UserData(
-            bookmarkedNewsResources = setOf("N1"),
-            viewedNewsResources = setOf("N1"),
-            followedTopics = setOf("T1", "T2"),
-            themeBrand = ThemeBrand.DEFAULT,
-            darkThemeConfig = DarkThemeConfig.FOLLOW_SYSTEM,
-            useDynamicColor = false,
-            shouldHideOnboarding = true,
+    val userData: Flow<UserData> = flow {
+        emit(
+            UserData(
+                bookmarkedNewsResources = setOf("N1"),
+                viewedNewsResources = setOf("N1"),
+                followedTopics = setOf("T1", "T2"),
+                themeBrand = ThemeBrand.DEFAULT,
+                darkThemeConfig = DarkThemeConfig.FOLLOW_SYSTEM,
+                useDynamicColor = false,
+                shouldHideOnboarding = true,
+            )
         )
-    )
+    }
 }
