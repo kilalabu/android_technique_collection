9a2de4ca578aaf12dd202ab912e67f2c64a053e2
kuwa <h.kuwabara1103@gmail.com>
2025-03-09 11:07:24 +0900

    簡易UIを実装

--- app/src/main/java/com/example/android_technique_collection/feature/foryou/ForYouScreen.kt
+++ app/src/main/java/com/example/android_technique_collection/feature/foryou/ForYouScreen.kt
@@ -1,10 +1,13 @@
 package com.example.android_technique_collection.feature.foryou
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -12,12 +15,9 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.*
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun ForYouScreen(
@@ -69,17 +69,20 @@ fun ForYouScreen(
                         items = feedState.feed,
                         key = { it.id },
                         contentType = { "newsFeedItem" },
-                    ) {
-                        Column {
-                            Text(text = it.toString())
-                            HorizontalDivider(
-                                modifier = Modifier.padding(vertical = 16.dp),
-                            )
-                        }
+                    ) { userNewsResource ->
+                        NewsResourceCard(
+                            userNewsResource = userNewsResource,
+                            isBookmarked = userNewsResource.isSaved,
+                            onClick = {},
+                            onToggleBookmark = {},
+                            onTopicClick = {},
+                            modifier = Modifier
+                                .padding(8.dp)
+                                .animateItem(),
+                        )
                     }
                 }
             }
         }
     }
-
 }
