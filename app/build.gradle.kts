8578c9ecd055f189c02cd3d6afc7a29b0671c1c7
kuwa <h.kuwabara1103@gmail.com>
2025-03-11 20:56:52 +0900

    DataStore Protoを導入

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.hiltAndroid)
     alias(libs.plugins.roborazzi)
+    alias(libs.plugins.protobuf)
     kotlin("plugin.power-assert") version libs.versions.kotlin
 }
 
@@ -65,6 +66,26 @@ android {
     }
 }
 
+// Setup protobuf configuration, generating lite Java and Kotlin classes
+protobuf {
+    protoc {
+        // Download from repositories
+        artifact = libs.protobuf.protoc.get().toString()
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.builtins {
+                register("java") {
+                    option("lite")
+                }
+                register("kotlin") {
+                    option("lite")
+                }
+            }
+        }
+    }
+}
+
 dependencies {
 
     implementation(libs.androidx.core.ktx)
@@ -84,6 +105,9 @@ dependencies {
     implementation(libs.converter.moshi)
     implementation(libs.moshi.kotlin)
     implementation(libs.coil)
+
+    implementation(libs.datastore)
+    api(libs.protobuf.kotlin.lite)
 
     testImplementation(libs.junit)
     testImplementation(libs.robolectric)
