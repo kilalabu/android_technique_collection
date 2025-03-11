137d09a92f3222cd0c5a1a06e5a970b95eac09b8
kuwa <h.kuwabara1103@gmail.com>
2025-03-11 21:01:50 +0900

    Serializer作成

---
+++ app/src/main/java/com/example/android_technique_collection/core/datastore/UserPreferencesSerializer.kt
@@ -0,0 +1,25 @@
+package com.example.android_technique_collection.core.datastore
+
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.Serializer
+import com.example.android_technique_collection.UserPreferences
+import com.google.protobuf.InvalidProtocolBufferException
+import java.io.InputStream
+import java.io.OutputStream
+import javax.inject.Inject
+
+// 公式の通りに実装 https://developer.android.com/topic/libraries/architecture/datastore
+class UserPreferencesSerializer @Inject constructor() : Serializer<UserPreferences>{
+    override val defaultValue: UserPreferences = UserPreferences.getDefaultInstance()
+
+    override suspend fun readFrom(input: InputStream): UserPreferences =
+        try {
+            UserPreferences.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
+
+    override suspend fun writeTo(t: UserPreferences, output: OutputStream) {
+        t.writeTo(output)
+    }
+}
