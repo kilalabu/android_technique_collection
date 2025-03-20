082640066dcc1d453f039ce6f8bcaa0bb11b08e5
kuwa <h.kuwabara1103@gmail.com>
2025-03-20 09:04:36 +0900

    bookmarkテストを追加

--- app/src/test/java/com/example/android_technique_collection/core/data/repository/OfflineFirstUserDataRepositoryTest.kt
+++ app/src/test/java/com/example/android_technique_collection/core/data/repository/OfflineFirstUserDataRepositoryTest.kt
@@ -1,12 +1,16 @@
 package com.example.android_technique_collection.core.data.repository
 
+import com.example.android_technique_collection.UserPreferences
 import com.example.android_technique_collection.core.datastore.NiaPreferencesDataSource
 import com.example.android_technique_collection.core.datastore.sampleUserData
+import com.example.android_technique_collection.core.testing.datastore.InMemoryDataStore
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
 import kotlin.test.assertEquals
@@ -21,7 +25,9 @@ class OfflineFirstUserDataRepositoryTest {
 
     @Before
     fun setup() {
-        niaPreferencesDataSource = NiaPreferencesDataSource()
+        niaPreferencesDataSource = NiaPreferencesDataSource(
+            InMemoryDataStore(UserPreferences.getDefaultInstance())
+        )
         subject = OfflineFirstUserDataRepository(
             niaPreferencesDataSource = niaPreferencesDataSource
         )
@@ -33,6 +39,38 @@ class OfflineFirstUserDataRepositoryTest {
             assertEquals(
                 sampleUserData,
                 subject.userData.first()
+            )
+        }
+    }
+
+    @Test
+    fun offlineFirstUserDataRepository_bookmark_news_resource_logic_delegates_to_nia_preferences() {
+        testScope.runTest {
+            subject.setNewsResourceBookmarked(newsResourceId = "0", bookmarked = true)
+
+            assertEquals(
+                setOf("0"),
+                subject.userData
+                    .map { it.bookmarkedNewsResources }
+                    .first()
+            )
+
+            subject.setNewsResourceBookmarked(newsResourceId = "1", bookmarked = true)
+
+            assertEquals(
+                setOf("0", "1"),
+                subject.userData
+                    .map { it.bookmarkedNewsResources }
+                    .first()
+            )
+
+            assertEquals(
+                niaPreferencesDataSource.userData
+                    .map { it.bookmarkedNewsResources }
+                    .first(),
+                subject.userData
+                    .map { it.bookmarkedNewsResources }
+                    .first()
             )
         }
     }
