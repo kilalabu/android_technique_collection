2574e3b6d3f10a0910839460c0a374c310a4b016
kuwa <h.kuwabara1103@gmail.com>
2025-04-01 21:58:45 +0900

    DetailにBackEventを追加

--- app/src/main/java/com/example/android_technique_collection/feature/inbox/DetailScreen.kt
+++ app/src/main/java/com/example/android_technique_collection/feature/inbox/DetailScreen.kt
@@ -21,6 +21,7 @@ import com.slack.circuit.runtime.Navigator
 import com.example.android_technique_collection.data.repository.EmailRepositoryImpl
 import com.example.android_technique_collection.domain.model.inbox.Email
 import com.slack.circuit.runtime.CircuitContext
+import com.slack.circuit.runtime.CircuitUiEvent
 import com.slack.circuit.runtime.CircuitUiState
 import com.slack.circuit.runtime.presenter.Presenter
 import com.slack.circuit.runtime.screen.Screen
@@ -28,23 +29,35 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class DetailScreen(val emailId: String) : Screen {
-    data class State(val email: Email) : CircuitUiState
+    data class State(
+        val email: Email,
+        val eventSink: (Event) -> Unit
+    ) : CircuitUiState
+
+    sealed class Event : CircuitUiEvent {
+        data object BackClicked : Event()
+    }
 }
 
 class DetailPresenter(
     private val screen: DetailScreen,
+    private val navigator: Navigator,
     private val emailRepository: EmailRepositoryImpl
 ) : Presenter<DetailScreen.State> {
     @Composable
     override fun present(): DetailScreen.State {
         val email = emailRepository.getEmail(screen.emailId)
-        return DetailScreen.State(email)
+        return DetailScreen.State(email) { event ->
+            when (event) {
+                DetailScreen.Event.BackClicked -> navigator.pop()
+            }
+        }
     }
 
     class Factory(private val emailRepository: EmailRepositoryImpl) : Presenter.Factory {
         override fun create(screen: Screen, navigator: Navigator, context: CircuitContext): Presenter<*>? {
             return when (screen) {
-                is DetailScreen -> return DetailPresenter(screen, emailRepository)
+                is DetailScreen -> return DetailPresenter(screen, navigator, emailRepository)
                 else -> null
             }
         }
@@ -62,7 +75,7 @@ fun EmailDetail(state: DetailScreen.State, modifier: Modifier = Modifier) {
                 title = { Text(subject) },
                 navigationIcon = {
                     IconButton(onClick = {
-                        //state.eventSink(DetailScreen.Event.BackClicked)
+                        state.eventSink(DetailScreen.Event.BackClicked)
                     }) {
                         Icon(Icons.AutoMirrored.Default.ArrowBack, contentDescription = "Back")
                     }
