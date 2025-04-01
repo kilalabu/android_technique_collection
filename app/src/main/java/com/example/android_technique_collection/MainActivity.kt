038937ac417460363b4c8bd0b1b0e67d62ddf3ab
kuwa <h.kuwabara1103@gmail.com>
2025-04-01 21:38:18 +0900

    Activityと繋ぎこみ

--- app/src/main/java/com/example/android_technique_collection/MainActivity.kt
+++ app/src/main/java/com/example/android_technique_collection/MainActivity.kt
@@ -10,45 +10,38 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.android_technique_collection.data.repository.EmailRepositoryImpl
 import com.example.android_technique_collection.feature.chart.ChartScreen
 import com.example.android_technique_collection.feature.home.HomeScreen
+import com.example.android_technique_collection.feature.inbox.Inbox
+import com.example.android_technique_collection.feature.inbox.InboxPresenter
+import com.example.android_technique_collection.feature.inbox.InboxScreen
 import com.example.android_technique_collection.feature.searchphoto.SearchPhotoScreen
 import com.example.android_technique_collection.ui.common.theme.Android_technique_collectionTheme
 import com.example.android_technique_collection.ui.common.route.ScreenRoute
+import com.slack.circuit.backstack.rememberSaveableBackStack
+import com.slack.circuit.foundation.Circuit
+import com.slack.circuit.foundation.CircuitCompositionLocals
+import com.slack.circuit.foundation.NavigableCircuitContent
+import com.slack.circuit.foundation.rememberCircuitNavigator
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val emailRepository = EmailRepositoryImpl()
+        val circuit: Circuit =
+            Circuit.Builder()
+                .addPresenterFactory(InboxPresenter.Factory(emailRepository))
+                .addUi<InboxScreen, InboxScreen.State> { state, modifier -> Inbox(state, modifier) }
+                .build()
         setContent {
             Android_technique_collectionTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    val navController = rememberNavController()
-                    NavHost(
-                        navController = navController,
-                        startDestination = ScreenRoute.HomeScreen.route
-                    ) {
-                        composable(ScreenRoute.HomeScreen.route) {
-                            HomeScreen(
-                                onNavigateToSearch = {
-                                    navController.navigate(ScreenRoute.SearchScreen.route)
-                                },
-                                onNavigateToChart = {
-                                    navController.navigate(ScreenRoute.ChartScreen.route)
-                                }
-                            )
-                        }
-                        composable(ScreenRoute.SearchScreen.route) {
-                            SearchPhotoScreen()
-                        }
-                        composable(ScreenRoute.ChartScreen.route) {
-                            ChartScreen()
-                        }
-                    }
+                val backStack = rememberSaveableBackStack(root = InboxScreen)
+                val navigator = rememberCircuitNavigator(backStack)
+                CircuitCompositionLocals(circuit) {
+                    NavigableCircuitContent(navigator, backStack)
                 }
             }
         }
